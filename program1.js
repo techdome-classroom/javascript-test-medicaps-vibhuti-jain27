748114f3906552da592345c6995f50520859a574
unknown <vibhuti-jain27>
2024-04-25 14:02:48 +0530

    Commit Successfully

--- program1.js
+++ program1.js
@@ -1,5 +1,21 @@
 function longestSubstring(s) {
-    // Implementation of longestSubstring function
+    let maxLength = 0;
+    let left = 0;
+    let count = {};
+
+    for (let right = 0; right < s.length; right++) {
+        let c = s[right];
+        count[c] = (count[c] || 0) + 1;
+        
+        while (count[c] > 1) {
+            count[s[left]] -= 1;
+            left++;
+        }
+        
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+    
+    return maxLength;
 }
 
 module.exports = { longestSubstring };
--- program2.js
+++ program2.js
@@ -1,6 +1,15 @@
 function smallestMissingPositiveInteger(nums) {
 
   // Implement the function smallest_missing_positive_integer
+  const numSet = new Set(nums);
+
+    // Start from 1 and iterate until finding the smallest missing positive integer
+    for (let i = 1; i <= nums.length + 1; i++) {
+        if (!numSet.has(i)) {
+            return i;
+        }
+    }
+
 
 }
 
